4a1bd014a0f879f93f55942ed1b0052490615964
Sudharsan Ravikumar <supersudh@gmail.com>
2023-08-17 11:16:20 +0530

    Add About page

--- src/app/Providers.js
+++ src/app/Providers.js
@@ -5,7 +5,9 @@ import { ThemeProvider } from 'next-themes';
 export default function Providers({ children }) {
   return (
     <ThemeProvider enableSystem attribute="class">
-      <div className="dark:bg-gray-700 dark:text-gray-200 text-gray-700 transition-colors duration-300 min-h-screen">
+      <div
+        className="dark:bg-gray-700 dark:text-gray-200 text-gray-700 transition-colors duration-300 min-h-screen"
+      >
         {children}
       </div>
     </ThemeProvider>
